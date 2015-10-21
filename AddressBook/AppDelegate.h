471b880cb8492d69a371d88828c89a6592f35b48
Admin <admin@Mac-Admin.local>
2015-10-21 10:09:07 +0300

    Initial Commit

---
+++ AddressBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AddressBook
+//
+//  Created by Admin on 21.10.15.
+//  Copyright (c) 2015 Savinov_Jr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AddressBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AddressBook
+//
+//  Created by Admin on 21.10.15.
+//  Copyright (c) 2015 Savinov_Jr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
